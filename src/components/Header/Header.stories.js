7ce4fb45ef865c8865ed0150b82630fd71784cbf
Agustin Barrio <agusbarrioeastman@gmail.com>
2022-07-15 00:28:49 -0300

    install storybooks

---
+++ src/components/Header/Header.stories.js
@@ -0,0 +1,17 @@
+// Button.stories.js|jsx
+
+import React from 'react';
+
+import Header from './Header';
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => <Header {...args} />;
+
+//👇 Each story then reuses that template
+export const Primary = Template.bind({});
+Primary.args = {};
