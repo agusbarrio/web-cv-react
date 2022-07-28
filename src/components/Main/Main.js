93fec0783690e99702ca0889c8a8bdcbdea625a6
Agustin Barrio <agusbarrioeastman@gmail.com>
2022-07-28 01:32:04 -0300

    responsive hero video

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,25 @@
 import './Main.scss';
-
+import { useState, useEffect } from 'react';
+import { useMediaQuery } from '@mui/material';
+import { MEDIAQUERIES } from '../../constants';
 function Main() {
+  const [videoSrc, setVideoSrc] = useState('movil-video.mp4');
+  const isMobile = useMediaQuery(MEDIAQUERIES.xs);
+  useEffect(() => {
+    if (isMobile) setVideoSrc('movil-video.mp4');
+    else setVideoSrc('video.mp4');
+  }, [isMobile]);
   return (
     <main data-component="Main">
-      <section className="page-section"></section>
+      <section className="page-section">
+        <video
+          className="hero-video"
+          autoPlay={true}
+          muted={true}
+          type="video/mp4"
+          src={videoSrc}
+        ></video>
+      </section>
       <section className="page-section"></section>
       <section className="page-section"></section>
       <section className="page-section"></section>
